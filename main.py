595028e3556d6b71df0aab10221a53c0ed000004
Eduardo-Geandre <eduardogeandreoliva@gmail.com>
2022-06-26 19:39:51 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+current_date = date.today()
+
+data_nascimento= int(input("Ano de nascimento:"))
+data_actual = current_date.year
+idade =data_actual-data_nascimento
+
+print(idade)
+
+if idade <=18:
+    print('Voce é menor de idade, não pode entrar')
+
+else:
+    print('Voce é de maior, boa festa')
